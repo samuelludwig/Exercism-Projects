2e204504adbd95ffd4fbc5eb257b924a903b4ee5
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-02-20 19:26:59 -0500

    adds start of nucleotide-count program

---
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -0,0 +1,15 @@
+(ns nucleotide-count)
+
+(defn count [nucleotide strand]
+  "Evaluates to the count of given nucleotide in a given strand
+nucleotide will be represented "
+  (loop [nucleotide nucleotide
+         strand     strand]
+    
+    )
+  )
+
+(defn nucleotide-counts [strand]
+  (loop [strand strand]
+    )
+)
