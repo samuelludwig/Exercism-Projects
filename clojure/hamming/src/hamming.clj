8c4d0f0114656006a7b307ee038cee470458ac5f
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-02-23 20:48:51 -0500

    finishes clojure version of hamming problem

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -1,5 +1,14 @@
 (ns hamming)
 
-(defn distance [strand1 strand2] ; <- arglist goes here
-  ;; your code goes here
-)
+(defn distance [strand1 strand2]
+  (if
+   (not (= (count strand1) (count strand2))) nil
+   (loop [s1 strand1
+          s2 strand2
+          distance 0]
+     (cond
+       (= (count s1) 0) distance
+       (= (first s1) (first s2)) (recur (rest s1) (rest s2) (+ distance 0))
+       :else (recur (rest s1) (rest s2) (+ distance 1))))))
+
+(distance "ACCAGGG" "ACTATGG")
