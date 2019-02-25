6e6c8553adb6a145fb2c6b03cd55de1a1f83f57c
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-02-25 09:31:33 -0500

    finishes clojure version of armstrong numbers problem
    < 20 lines, I'm happy with that for now

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,17 @@
 (ns armstrong-numbers)
 
-(defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(require '[clojure.math.numeric-tower :as mat])
+
+(defn armstrong? [num]
+  (if
+   (= num (loop [num-sum 0
+                 num-pos 0
+                 num-len (count (str num))
+                 num-seq (map #(- % 48) (map int (seq (char-array (str num)))))]
+            (if (= num-pos num-len) num-sum
+                (recur (+ num-sum (mat/expt (first num-seq) num-len))
+                       (inc num-pos)
+                       num-len
+                       (rest num-seq))))) 
+    true
+    false))
