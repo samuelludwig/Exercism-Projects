1d91afc3cf32c76c61dfecc78d26b77cfc039ee7
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-02-27 15:44:02 -0500

    adds progress to clojure version of the clock problem the clock definition function is completed, the to-string and add function are all that remain

--- clojure/clock/src/clock.clj
+++ clojure/clock/src/clock.clj
@@ -1,13 +1,16 @@
 (ns clock)
 
-(defn clock->string [clock] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn clock->string [clock]
+  ;; turn vector passed by clock into a string
+  (let [hours    (str (nth clock 0))
+        minutes  (str (nth clock 1))]
+    (concat hours ":" minutes)))
 
-(defn clock [hours minutes] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn clock [hours minutes]
+  (let [tru-min (mod minutes 60)
+        hour-ov (quot minutes 60)
+        tru-hrs (mod (+ hours hour-ov) 24)]
+    (vector tru-hrs tru-min)))
 
-(defn add-time [clock time] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn add-time [clock time]
+  )
