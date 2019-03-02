57e6839235337724e9204e768620a1cd5ae84159
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-03-02 09:41:43 -0500

    refines clojure armstrong numbers program
    My mentor was 100% correct, the if statement returning true or false was pointless;
    I was already using a predicate as the argument, so I can just use the predicate
    by itself to return what I want.

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -3,15 +3,12 @@
 (require '[clojure.math.numeric-tower :as mat])
 
 (defn armstrong? [num]
-  (if
-   (= num (loop [num-sum 0
-                 num-pos 0
-                 num-len (count (str num))
-                 num-seq (map #(- % 48) (map int (seq (char-array (str num)))))]
-            (if (= num-pos num-len) num-sum
-                (recur (+ num-sum (mat/expt (first num-seq) num-len))
-                       (inc num-pos)
-                       num-len
-                       (rest num-seq))))) 
-    true
-    false))
+  (= num (loop [num-sum 0
+                num-pos 0
+                num-len (count (str num))
+                num-seq (map #(- % 48) (map int (seq (char-array (str num)))))]
+           (if (= num-pos num-len) num-sum
+               (recur (+ num-sum (mat/expt (first num-seq) num-len))
+                      (inc num-pos)
+                      num-len
+                      (rest num-seq))))))
