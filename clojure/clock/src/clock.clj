48e211ebbb7aad6b4647a158f651f538644a3c4c
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-03-02 09:25:28 -0500

    finishes clock.clj problem
    Oh boy, i did do a lot of unneccesary work on this one, looking at the other submissions certainly makes me realize that.
    I had no idea about the format function, but I should have figured something like that existed. My approach to
    this was messy, I will try to keep what I've learned in mind.

--- clojure/clock/src/clock.clj
+++ clojure/clock/src/clock.clj
@@ -2,15 +2,24 @@
 
 (defn clock->string [clock]
   ;; turn vector passed by clock into a string
-  (let [hours    (str (nth clock 0))
-        minutes  (str (nth clock 1))]
-    (concat hours ":" minutes)))
+  (let [hours   (first clock)
+        minutes (second clock)]
+    (def ret-str "")
+    (if (< hours 10) (def ret-str (concat ret-str "0")))
+    (def ret-str (concat ret-str (str hours) ":"))
+    (if (< minutes 10) (def ret-str (concat ret-str "0")))
+    (def ret-str (concat ret-str (str minutes)))
+    (apply str ret-str)))
 
 (defn clock [hours minutes]
   (let [tru-min (mod minutes 60)
         hour-ov (quot minutes 60)
-        tru-hrs (mod (+ hours hour-ov) 24)]
+        tru-hrs (if (< minutes 0) (mod (- (+ hours hour-ov) 1) 24) (mod (+ hours hour-ov) 24))]
     (vector tru-hrs tru-min)))
 
 (defn add-time [clock time]
-  )
+  (let [tru-min (mod (+ (second clock) time) 60)
+        hour-ov (quot (+ (second clock) time) 60)
+        tru-hrs (if (and (< time 0) (> (* time -1) (second clock))) (mod (- (+ (first clock) hour-ov) 1) 24) (mod (+ (first clock) hour-ov) 24))]
+    (vector tru-hrs tru-min)))
+
