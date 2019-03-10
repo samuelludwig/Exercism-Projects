9c6ef6d490bd36cb27d3978020767f292c66885d
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-03-09 20:12:58 -0500

    adds Pangram.cs program

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -4,6 +4,25 @@ public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        char[] alpha = new char[26];
+        int initASCIINum = 97;
+        for(int i = 0; i < alpha.Length; i++) {
+            alpha[i] = Convert.ToChar(i + initASCIINum);
+        } //Programmatically creating the alphabet because I am lazy
+
+        char[] str = input.ToLower().ToCharArray();
+
+        int lettersFound = 0;
+        for(int i = 0; i < alpha.Length; i++) {
+            if(Array.Exists(str, element => element == alpha[i])) {
+                lettersFound++;
+            }
+        }
+
+        if(lettersFound == 26) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
