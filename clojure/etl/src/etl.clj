e79e3e949bae865361af387e120c48c480f697c4
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-03-11 06:46:33 -0400

    finishes etl clojure program

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,5 +1,23 @@
-(ns etl)
+(ns etl
+  (:require [clojure.string :as str]))
 
-(defn transform [source] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn transform [source]
+  (loop [source      source
+         new-table   {}
+         point-vals  (keys source)
+         letters     (vals source)
+         this-point  (first point-vals)
+         this-letter (first letters)]
+
+    (if (< 0 (count point-vals))
+      (recur
+       source
+       (merge new-table
+              (apply hash-map
+                     (into (vector)
+                           (interleave (map str/lower-case this-letter) (repeat this-point)))))
+       (rest point-vals)
+       (rest letters)
+       (first (rest point-vals))
+       (first (rest letters)))
+      new-table)))
