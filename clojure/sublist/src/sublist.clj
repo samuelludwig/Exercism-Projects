19e5c71175df4e719db08dd80535568e23494633
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-03-13 09:36:13 -0400

    finishes sublist clojure problem
    I think I am finally getting to a point where I need to stop being afraid to split things up into other functions; I will try to keep this lesson in mind. And I must stop forgetting partition exists, its too useful!

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -1,5 +1,30 @@
 (ns sublist)
 
-(defn classify [list1 list2] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn classify [list1 list2]
+  (let [count1 (count list1)
+        count2 (count list2)]
+    (cond
+      (= list1 list2) :equal
+      (and (< count1 count2)
+           (loop [start-pos 0
+                  end-pos   count1
+                  sub-2     (subvec list2 start-pos end-pos)]
+             (cond
+               (= list1 sub-2) true
+               (and (= end-pos count2) (not= list1 sub-2)) false
+               :else (recur
+                      (inc start-pos)
+                      (inc end-pos)
+                      (subvec list2 (inc start-pos) (inc end-pos)))))) :sublist
+      (and (> count1 count2)
+           (loop [start-pos 0
+                  end-pos   count2
+                  sub-1     (subvec list1 start-pos end-pos)]
+             (cond
+               (= list2 sub-1) true
+               (and (= end-pos count1) (not= list2 sub-1)) false
+               :else (recur
+                      (inc start-pos)
+                      (inc end-pos)
+                      (subvec list1 (inc start-pos) (inc end-pos)))))) :superlist
+      :else :unequal)))
