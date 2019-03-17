275fff491d1926564ae4f040da6650af11543e44
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-03-17 13:47:15 -0400

    finishes rot_cipher for elixir

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -8,5 +8,17 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    String.to_charlist(text) |> Enum.map(fn(x) -> shift_c(x, shift) end) |> to_string()
+  end
+
+  @spec shift_c(character :: char, shift :: integer) :: char
+  defp shift_c(char, shift) do
+    cond do
+      (char >= ?a and char <= ?z and char + shift <= ?z) -> char + shift
+      (char >= ?a and char <= ?z) -> char + shift - 26
+      (char >= ?A and char <= ?Z and char + shift <= ?Z) -> char + shift
+      (char >= ?A and char <= ?Z) -> char + shift - 26
+      true -> char
+    end
   end
 end
